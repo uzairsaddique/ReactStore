62754b6231f7f074bcecb385053df368470ed2c4
Uzair Siddique <usi@facelift-bbt.com>
2021-02-01 10:32:51 +0500

    somethings is here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+     <div className="row">
+       <div className="col-6">uzair</div>
+       <div className="col-6">umair</div>
+     </div>
+
+   </div>
   );
 }
 
-export default App;
+export default App; 
